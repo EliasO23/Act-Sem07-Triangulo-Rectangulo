d37d2793a563dcdded9ffcffcb1d9f9165832967
EliasO23 <124007307+EliasO23@users.noreply.github.com>
2023-03-10 18:17:37 -0600

    Proyecto

---
+++ TrianguloFor/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("\nBIENVENIDO AL CREADOR DE TRIANGULOS");
+Console.WriteLine("             RECTANGULOS             ");
+Console.WriteLine("\nIngrese la altura del triangulo: ");
+int altura = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("\n-----------------------------------");
+Console.WriteLine("             Triangulo   ");
+Console.WriteLine("-----------------------------------\n");
+for (int i = 1; i <= altura; i++){
+    for (int j = 1; j <= i; j++){
+        Console.Write(" *");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("\n----------------------------------\n");
+
+Console.WriteLine("¡Gracias por viisitar el creador \nde tringulos rectangulos!\n");
+
+
+
+
